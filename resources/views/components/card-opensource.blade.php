3486d252da52ef1682484b71ad6f394a571365f5
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-10-08 17:47:26 +0700

    edit database

--- resources/views/components/card-opensource.blade.php
+++ resources/views/components/card-opensource.blade.php
@@ -10,7 +10,7 @@
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                 </svg></button>
             </div>
-                <img src="{{url('storage/'.$project->image_poster)}}" class=" w-full   object-fill  rounded-2xl transition-transform transform hover:scale-110">
+                <img src="{{asset('storage/'.$project->image_poster)}}" class=" w-full   object-fill  rounded-2xl transition-transform transform hover:scale-110">
             </div>
             <div class="flex-auto justify-evenly">
             <div class="flex flex-wrap ">
