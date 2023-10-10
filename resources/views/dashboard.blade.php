3a90967cc44add1439099644b3cbbf5848d8dcf0
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-10-10 17:17:03 +0700

    edit dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -31,20 +31,7 @@
       </div>
    </x-container>
   
-   {{-- <div class="m-8 p-8 ">
-    <h1 class="text-white font-black text-xl mb-6">My Project</h1>
-    <div class="border border-dashed group bg-gray-900/30 py-20 h-full  px-4 flex flex-col space-y-2 items-center  justify-center cursor-pointer rounded-md hover:bg-gray-900/40 hover:smooth-hover">
-        <a href="/marketplace/create" class=" text-white/50 group-hover:text-white group-hover:smooth-hover flex w-20 h-20 rounded-full items-center justify-center" href="#">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
-          </svg>
-        </a>
-        <a href="/marketplace/create" class="text-white/50 group-hover:text-white group-hover:smooth-hover text-center">
-          Create Project
-        </a>
-    
-      </div>
-</div> --}}
+
 
 
 
